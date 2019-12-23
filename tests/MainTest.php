19e3703e90c9b8a0611130ded13eaf414854a9a0
Arkadij Kuzhel <arkadij@madepeople.se>
2019-12-23 11:57:28 +0200

    Initialize PHPUnit
     * Use PHP 7.0 compatible version PHPUnit 6.5

---
+++ tests/MainTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright © Madepeople, Inc. All rights reserved.
+ *
+ * @author    Arkadij Kuzhel <arkadij@madepeople.se>
+ * @created   23.12.19
+ */
+declare(strict_types=1);
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MainTest
+ *
+ */
+class MainTest extends TestCase
+{
+    public function testFake()
+    {
+        $this->assertTrue(true, "PHPUnit doesn't work");
+    }
+}
